bd76291fab94190d242d4d73bd14022392fea786
AliceSantoi <alicelira2016@gmail.com>
2023-11-19 20:20:09 -0300

    adicionando procedimentos

--- programas_sequenciais_simples/dois_nums/dois_nums.c
+++ programas_sequenciais_simples/dois_nums/dois_nums.c
@@ -1,43 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//continuar em: linha(54) - procurar como limpar a tela apos entrada de valores
 
-int main(){
-    float num1,num2,soma,calc,qua1,soQua,senDif,modNum1;
+
+void linha(){
+    printf("--------------------------------\n");
+}
+
+//soma
+void soma(float a, float b){
+    float som;
+    som = a + b;
+    printf("A soma de %.1f + %.1f e: %.1f\n",a,b,som);
+    linha();
+}
+
+//O produto do primeiro número pelo quadrado do segundo
+void produto(float c, float d){
+    float calc;
+    calc = c * pow(d,2);
+    printf("O produto do primeiro numero pelo quadrado do segundo: %.1f\n",calc);
+     linha();
+}
+
+// O quadrado do primeiro número
+ void quadrado(float e, float f){
+    float qua1; 
+    qua1 = pow(e,2);
+    printf("O quadrado do primeiro numero: %.1f\n",qua1);
+     linha();
+ } 
+
+ //A raiz quadrada da soma dos quadrados
+ void somQua(float g, float h){
+    float soQua;
+    soQua = sqrt(pow(g,2) + pow(h,2));
+    printf("A raiz quadrada da soma dos quadrados: %.1f\n",soQua);
+     linha();
+ } 
+
+ // O seno da diferença do primeiro número pelo segundo;
+ //"cos(a – b) = cos(a) · cos (b) + sen (a) · sen (b)"
+void senoDif(float i, float j){
+    float senDif;
+    senDif = cos(i) * cos(j) + sin(i) * sin(j);
+    printf("O seno da diferença do primeiro numero pelo segundo: %.1f\n",senDif);
+     linha();
+}
+
+// O módulo do primeiro número.
+void modNumOne(float k, float l){
+    float modNum1;
+     modNum1 = abs(k);
+     printf("O módulo do primeiro numero: %.1f\n",modNum1);
+      linha();
+}
+
+void main(){
+    float num1,num2;
 
     printf("Digite o primeiro numero: ");
     scanf("%f",&num1);
 
     printf("Agora o segundo: ");
     scanf("%f",&num2);
+    system("cls");
+
+    soma(num1,num2); //como reduzir o número de printfs?
+    produto(num1,num2);
+    quadrado(num1,num2);
+    somQua(num1,num2);
+    senoDif(num1,num2);
+    modNumOne(num1,num2);
+
     
-    //soma
-    soma = num1 + num2;
-
-    //O produto do primeiro número pelo quadrado do segundo
-    calc = num1 * pow(num2,2);
-''
-    // O quadrado do primeiro número
-    qua1 = pow(num1,2);
-
-    //A raiz quadrada da soma dos quadrados
-    soQua = sqrt(qua1 + pow(num2,2));
-
-    // O seno da diferença do primeiro número pelo segundo;
-    //"cos(a – b) = cos(a) · cos (b) + sen (a) · sen (b)"
-
-    senDif = cos(num1) * cos(num2) + sin(num1) * sin(num2);
-
-    // O módulo do primeiro número.
-    modNum1 = abs(num1);
-
-    printf("%.1f\n",soma); //como reduzir o número de printfs?
-    printf("%.1f\n",calc);
-    printf("%.1f\n",qua1);
-    printf("%.1f\n",soQua);
-    printf("%.1f\n",senDif);
-    printf("%.1f",modNum1);
-    return 0;
 }
 
 
