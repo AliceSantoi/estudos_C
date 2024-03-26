c0139c45fca5d25e29406ba5dd50beb6bc726089
AliceSantoi <alicelira2016@gmail.com>
2024-03-26 15:16:23 -0300

    código maior_vetor.c

--- listas/lista_IX/4_maior-vetor.c
+++ listas/lista_IX/4_maior-vetor.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <locale.h>
 
-/*4. Escreva um programa que solicita e armazena dez números inteiros em um vetor e,
+/*4. Escreva um programa que solicita e armazena dez nï¿½meros inteiros em um vetor e,
 ao final, informa o maior valor encontrado no vetor*/
 
 int main(){
-	
-	
+	int num[10],i,maior = 0;
+
+	for(i = 0;i < 10;i++){
+		printf("Insira o %d valor: ",i + 1);
+		scanf("%d",&num[i]);
+	}
+
+	for(i = 0;i < 10;i++){
+		if(num[i] > maior){
+			maior = num[i];
+		}
+	}
+	printf("O maior Ã©: %d",maior);
 	
 	
 	
